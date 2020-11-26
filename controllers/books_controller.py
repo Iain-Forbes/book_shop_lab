6c0d33639fb64d2f1a2bc91dea898e87fa6caf73
Iain Forbes <i.forbes77@gmail.com>
2020-11-26 07:30:00 +0000

    odd bug

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, Blueprint, redirect, request
+from flask import Flask, render_template, Blueprint, redirect, request 
 from models.book import Book
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
 
-book_blueprint = Blueprint('books', __name__)
+books_blueprint = Blueprint('books', __name__)
 
 # INDEX
 # GET '/books'
-@book_blueprint.route("/books")
+@books_blueprint.route("/books")
 def book():  
     book = book_repository.select_all()
     return render_template("books/index.html", all_books=book)
@@ -24,21 +24,41 @@ def new_book():
 
 # CREATE
 # POST '/books'
-
-
-# SHOW
-# GET '/books/<id>'
+@books_blueprints.route("/books", methods=['POST'])
+def create_book():
+    title    = request.form['title']
+    genre = request.form['genre']
+    publisher   = request.form['publisher']
+    author  = author_repository.select(request.form['author_id'])
+    book = Book(title, genre, publisher, author)
+    book_repository.save(book)
+    return redirect('/books')
 
 
 # EDIT
 # GET '/books/<id>/edit'
-
+@books_blueprint.route("/books/<id>/edit", methods=['GET'])
+def edit_book(id):
+    book = book_repository.select(id)
+    authors = author_repository.select_all()
+    return render_template('books/edit.html', book = book, all_authors = authors)
 
 # UPDATE
 # PUT '/books/<id>'
-
-
+@books_blueprint.route("/books/<id>", methods=['POST'])
+def update_book(id):
+    title    = request.form['title']
+    genre = request.form['genre']
+    publisher   = request.form['publisher']
+    author  = author_repository.select(request.form['author_id'])
+    book = Book(title, genre, publisher, author, id)
+    print(book.author.full_name())
+    book_repository.update(book)
+    return redirect('/books')
 
 # DELETE
 # DELETE '/books/<id>'
-
+@books_blueprint.route("/books/<id>/delete", methods=['POST'])
+def delete_book(id):
+    book_repository.delete(id)
+    return redirect('/books')
